e92bae8ce173eb23f7cc3ba2819a0c2c52af4810
Kaaustubh pingle <Kaustubhpingale007@gmail.com>
2022-12-19 10:26:46 +0530

    multi-line jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+// MULTI LINE JSX ::: <App />
 function App() {
-  return  "Hello! i am kaaustubh pingle";
+  return (
+    <h1>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum sed maxime
+      quisquam inventore reprehenderit! Nobis ducimus, voluptatem cum at
+      placeat, quia facere ut natus ipsam id temporibus iusto velit officia?
+    </h1>
+  );
 }
 
 export default App;
